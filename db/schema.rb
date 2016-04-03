2558b75080ce54de1d841f5cdf5b022811004a7e
Djaka <djakapermana95@gmail.com>
2016-04-03 13:42:53 +0700

    menambahkan field

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403045039) do
+ActiveRecord::Schema.define(version: 20160403055352) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "explanattion"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "price"
   end
 
 end
